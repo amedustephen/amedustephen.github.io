8a33fd290c575171a22a93e2610be70e931e443a
amedustephen <amedustephen@hotmail.com>
2022-09-12 21:23:07 +0100

    First commit
    The site is fairly full fledged. Just committing it to github for the first time!

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+    new WOW().init();
+
+    $(".carousel").owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        responsive: {
+          0:{
+            items:1,
+            nav: false
+          },
+          600:{
+            items:2,
+            nav: false
+          },
+          1000:{
+            items:3,
+            nav: false
+          }
+        }
+      });   
+         
+  });
